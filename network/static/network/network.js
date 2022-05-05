86b061fd3153ad5dd8b85ed80ab5a1ebe5ebe304
Trenton Dunstan <trentondunstan@hotmail.com>
2022-05-05 15:44:32 +1000

    Began editing functionality, basic Javascript file added

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.edit').addEventListener('click', edit_post);
+    document.querySelectorAll('.edit').forEach(edit => {
+      edit.onclick = function() {
+        edit_post(this.id);
+        return false;
+        }
+      })
     // Waiting for post to be submitted
-    document.querySelector('#compose-post').onsubmit = send_post;
+    // document.querySelector('#compose-post').onsubmit = send_post;
   });
 
-function edit_post(post) {
-  console.log("DO EDIT STUFF");
-  document.querySelector('.post-body').innerHTML = "GET FUCKED";
-  return false
+function edit_post(editpost_id) {
+  const post_id = editpost_id.substring(5);
+  const originalPost = document.getElementById(`body_${post_id}`);
+  const originalPostText = originalPost.textContent;
+  let newPost = document.createElement('textarea');
+  newPost.innerHTML = originalPostText;
+  originalPost.append(newPost);
 }
 
 function send_post() {
